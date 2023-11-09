7436097409be848fc59f683926d21296d5f4b5e7
Chi-Shan Yu <chisamyu@seed.net.tw>
2023-11-09 18:35:01 +0800

    v18 script api key

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const cors = "https://cors-anywhere.herokuapp.com/"
 
-const baseUrl =
-  "https://script.google.com/macros/s/AKfycbzUvJmNkD6ho5dgCKL5gTLE9pcZc8wXhuxsAE5Uy17OxOBSxoZuPDC2tgdcShzRFr1g7w/exec";
+// const baseUrl =
+//   "https://script.google.com/macros/s/AKfycbzUvJmNkD6ho5dgCKL5gTLE9pcZc8wXhuxsAE5Uy17OxOBSxoZuPDC2tgdcShzRFr1g7w/exec";
+
+const baseUrl = "https://script.google.com/macros/s/AKfycbzDpeGPopnoGzs_2F0pK72vb9uJbg9f8uDEFPVFaUTqRoGgGczM_-a49H6vkqDCTMvYXQ/exec";
 
 export const getSheetData = async () => {
   try {

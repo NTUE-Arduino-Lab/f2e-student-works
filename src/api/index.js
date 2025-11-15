57d31ac78716bad0b2391adfd34419f4ef6f1800
Chi-Shan Yu <chisamyu@seed.net.tw>
2025-11-15 14:57:57 +0800

    add new gas url

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,9 @@ const cors = "https://cors-anywhere.herokuapp.com/"
 // const baseUrl =
 //   "https://script.google.com/macros/s/AKfycbzUvJmNkD6ho5dgCKL5gTLE9pcZc8wXhuxsAE5Uy17OxOBSxoZuPDC2tgdcShzRFr1g7w/exec";
 
-const baseUrl = "https://script.google.com/macros/s/AKfycbzW5fTHL6mV3UUeACV10FkdWak2Z5CNFuMYBF2GsfHsAGs4SGrxdUPSKRsamWQx5n8j/exec";
+// const baseUrl = "https://script.google.com/macros/s/AKfycbzW5fTHL6mV3UUeACV10FkdWak2Z5CNFuMYBF2GsfHsAGs4SGrxdUPSKRsamWQx5n8j/exec";
+
+const baseUrl = "https://script.google.com/macros/s/AKfycbztMYGVrkUe-V5JLIKMVqfnNULDTAcwK-6WDRjEkgE5Xpq-_k9-xjq_EggHqBEl_0M/exec"
 
 
 export const getSheetData = async () => {
